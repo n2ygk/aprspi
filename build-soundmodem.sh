20b02f22d1b93a547d2dcbe000186ddb1c17a0bf
Alan Crosswell <n2ygk@weca.org>
2023-11-14 13:04:11 -0500

    correct the build path

--- build-soundmodem.sh
+++ build-soundmodem.sh
@@ -6,15 +6,15 @@ sudo sed -i -e 's/^#\(deb-src.*\)$/\1/' /etc/apt/sources.list
 sudo apt-get update
 sudo apt build-dep -y soundmodem
 sudo apt-get install -y packaging-dev
-mkdir -p ${BASEDIR}/../src
-(cd ${BASEDIR}/../src; apt-get source soundmodem)
+mkdir -p ${BASEDIR}/src
+(cd ${BASEDIR}/src; apt-get source soundmodem)
 # figure out what directory name it's in:
 # e.g. soundmodem-0.20
 srcdir=`dpkg -s soundmodem|sed -n -e 's/^Source: \(soundmodem\) (\(.*\)-.*$/\1-\2/p'`
-cp fix-missing-hid-dinah-gpio.patch ${BASEDIR}/../src/${srcdir}/debian/patches/
-echo fix-missing-hid-dinah-gpio.patch >${BASEDIR}/../src/${srcdir}/debian/patches/series
+cp fix-missing-hid-dinah-gpio.patch ${BASEDIR}/src/${srcdir}/debian/patches/
+echo fix-missing-hid-dinah-gpio.patch >${BASEDIR}/src/${srcdir}/debian/patches/series
 (
-    cd ${BASEDIR}/../src/$srcdir
+    cd ${BASEDIR}/src/$srcdir
     export EMAIL="n2ygk@weca.org"
     dch -i "hidraw cm119b gpio ptt support"
     debuild -b -uc -us
