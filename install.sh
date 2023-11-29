daadb63c2541d9cb8344bb923ea09918a3cde74e
Alan Crosswell <alan@columbia.edu>
2023-11-29 18:14:51 -0500

    fail2ban

--- install.sh
+++ install.sh
@@ -65,6 +65,13 @@ apt-get install -y ufw
 ufw allow ssh
 ufw enable
 
+# block hosts making repeated failed attempts
+
+apt-get install -y fail2ban
+# fails to start because it can't find sshd log when backend = auto
+sed -i.bak -e s/^backend = auto/backend = systemd/ /etc/fail2ban/jail.conf
+systemctl restart fail2ban
+
 # we don't need to be multicasting who we are
 apt-get remove -y avahi-daemon
 apt autoremove -y
